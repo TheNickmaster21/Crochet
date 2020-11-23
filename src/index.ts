c0b4867646163d1004ac95d97e8edbace6cadad1
Nick Main <nickmaster24@gmail.com>
2020-11-22 22:46:55 -0500

    More jsdocs and changed the version to 1.0.0-beta.0.0

--- src/index.ts
+++ src/index.ts
@@ -18,10 +18,20 @@ function getErroringObject(errorMessage: string): unknown {
     );
 }
 
+/**
+ * This is the instance to import to use Crochet on the service.
+ *
+ * @throws CrochetServer can only be imported on the server and will error if used by a client.
+ */
 export const CrochetServer: CrochetServerImplementation = RunService.IsServer()
     ? new CrochetServerImplementation()
     : (getErroringObject('CrochetServer can only be used on the Server!') as CrochetServerImplementation);
 
+/**
+ * This is the instance to import to use Crochet on a client.
+ *
+ * @throws CrochetClient can only be imported on a client and will error if used by the server.
+ */
 export const CrochetClient: CrochetClientImplementation = RunService.IsClient()
     ? new CrochetClientImplementation()
     : (getErroringObject('CrochetClient can only be used on the Client!') as CrochetClientImplementation);
